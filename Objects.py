a1d38f6a1ff03d12e5b3047a398829e447af2ff9
juliusdeclercq <jlhdeclercq@gmail.com>
2023-03-31 01:02:01 +0200

    Update Objects.py

--- Objects.py
+++ Objects.py
@@ -363,41 +363,6 @@ def BoardingStrat(type):
     return list_order
 
 
-# IF IT DOES NOT WORK WITH DICTIONARIES, WE NEED TO CODE THIS AS AN OBJECT
-def PassengerGenerator(deltaT, row, seat):
-    # Simulated parameters
-    enter_time = random.uniform(1, 5)  # time spent by lining up and entering the airplane
-    has_luggage = rnd.choice((1, 0), p=[0.8, 0.2])  # logical if person has a luggage (1-yes, 0-no)
-    if has_luggage==1:
-        luggage_stash = random.uniform(6, 30)  # seconds spent with luggage stash
-    else:
-        luggage_stash = 0
-    walkspeed = random.uniform(0.27, 0.44)  # meter per second
-
-    # Calculated attributes and containing these attributes
-    walking_time = (0.7112/walkspeed) * (row + 3)
-    dict_res = {
-        "board_time_cum": 0,
-        "pass_start_relative": deltaT,
-        "pass_end_relative": deltaT,
-        "row_arrival_relative": deltaT,
-        "row": row,
-        "seat": seat,
-        "has_luggage": has_luggage,
-        "walkspeed": walkspeed,
-        "unit_walktime": 0.7112/walkspeed,
-        "aisle_pos": 0,
-        "timespent": {
-            "entrance": enter_time,
-            "walking": walking_time,
-            "waiting": 0,
-            "luggage_stash": luggage_stash,
-            "seating_time": 0,
-            "standup": 0
-        }
-    }
-    return dict_res
-
 
 def SeatingManager(arriving_passenger, seating_matrix):
     seat = arriving_passenger["seat"]
@@ -460,59 +425,196 @@ def SeatingManager(arriving_passenger, seating_matrix):
     arriving_passenger["board_time_cum"] = passenger_totaltime
     # Seat the arriving into seating matrix
     seating_matrix[row - 1][seat - 1] = arriving_passenger
-    # Seating completed
+    
+    
+# !!!!!!!!!!!!!!!!!!!!!!!!!!! FROM HERE THE CODE DEVIATES FROM DAVID'S LATEST PUBLISH ON 30/03   !!!!!!!!!!!!!!!!!!!!!!!!!!! 
+    
+    
+def PassengerGenerator(ID, row, seat):
+    # Simulated parameters
+    enter_time = random.uniform(1, 5)  # time spent by lining up and entering the airplane
+    has_luggage = rnd.choice((1, 0), p=[0.8, 0.2])  # logical if person has a luggage (1-yes, 0-no)
+    if has_luggage==1:
+        luggage_stash = random.uniform(6, 30)  # seconds spent with luggage stash
+    else:
+        luggage_stash = 0
+    walkspeed = random.uniform(0.27, 0.44)  # meter per second
+
+    # Calculated attributes and containing these attributes
+    walking_time = (0.7112/walkspeed) * row + 4*(0.4572/walkspeed)
+    dict_res = {
+        "board_time_cum": None,
+        "time_entrance": None,
+        "time_seated": None,
+        "row": row,
+        "seat": seat,
+        "has_luggage": has_luggage,
+        "walkspeed": {'aisle_begin' : size_aisle_begin/walkspeed,
+                      'aisle_seats' : size_aisle_seats/walkspeed},
+        "aisle_pos": None,
+        "seated": False,
+        "ID": ID,
+        "timespent": {
+            "entrance": enter_time,
+            "walking": walking_time,
+            "waiting": 0,
+            "luggage_stash": luggage_stash,
+            "seating_time": 0,
+            "standup": 0,
+            
+        }
+    }
+    return dict_res
 
 
-def WalkingImplement(aisle, ind_from, ind_to):
-    passenger = aisle[ind_from][0]
-    passenger["aisle_pos"] = ind_to
-    walktime = passenger["unit_walktime"]
-    aisle[ind_to][0] = passenger
-    aisle[ind_from][0] = {}
-    return aisle, walktime
+def New_arrival(passengers, aisle, current_time):
+    # If the first position in the aisle is vacant, give that position to the first passenger 
+    # in the passengers that had not yet been assigned a spot in the aisle.
+    if bool(aisle[0]):
+        print('Aisle is fully occupied, no new passenger can enter. THIS SHOULD NOT OCCUR')
+    for i in range(len(passengers)):
+        if passengers[i]['aisle_pos']==None:
+            while aisle[0] == 0:
+                passengers[i]['aisle_pos'] = 0
+                passengers[i]['Time_entrance'] = current_time
+                aisle[0] = 1
+                # Make a new entry for the passenger in the event list
+                pass_event = Passenger_event(passengers[i]['ID'], passengers[i]['walkspeed']['aisle_begin'])
+                events.append(pass_event)
 
-
-def AisleManager(type):
-    # Initialization
-    aisle = np.full((34, 1), {})
-    arrival_queue = BoardingStrat(type)
-    TIME = 0
-    # Initial boarding of first passenger
-    aisle[0][0] = PassengerGenerator(TIME, arrival_queue[0][0], arrival_queue[0][1])
-    aisle, walktime = WalkingImplement(aisle, 0, 1)
-    TIME += aisle[0][0]["timespent"]["entrance"] + walktime
-    arrival_queue.pop(0)
-
-    return None
-
-
-AisleManager("random")
+def Passenger_event(ID, timer):
+    return{'ID': ID, 'timer' : timer,}
 
 
 
 
-# Initialization and magic numbers
-aisle = np.full((35,1), {})
-print(aisle[34][0])
+# Magic numbers
 
-aisle[20] = PassengerGenerator(2, 24, 3)
-#print(aisle)
+# Number of seat rows on the plane
+nrows = 30
+# Seats per row
+nseats = 6
+# Total length of the aisle
+l_aisle = 35
 
-seating_matrix = np.full((30,6), {})
+# The space occupied on the aisle is larger next to the seats than on the first part.
+size_aisle_seats = 0.7112
+size_aisle_begin = 0.4572
 
-enter_time = random.randrange(1,6)
-#print(enter_time)
 
-walkspeed = random.uniform(0.27, 0.44)  # meter per second
-#print(walkspeed)
 
-lsit = [[2,3,4],
-        [3,4,5],
-        [4,5,6]]
 
-#print(lsit[1][1])
 
-t = [{}, {"e": 1}, {}]
-#print(bool(t[0]))
-#print(bool(t[1]))
-#print(bool(t[2]))
+# Initialization
+
+# Initializing passengers       (should make a function out of this)
+# Big change: save the passengers as a separate list that is created a priori. 
+# This process defines the boarding strategies, here a super stupid one is taken for coding simplicity.
+passengers = []
+ID=0
+for i in range(nrows):
+    for q in range(nseats):
+        ID+=1
+        passengers.append(PassengerGenerator(ID, i+1, q+1))
+        
+
+
+# Initializing the aisle
+# The aisle is a binary vector, where '1' designates occupancy and '0' vacancy.
+aisle = np.zeros(l_aisle)
+
+
+
+# Initializing global time
+current_time = 0
+
+
+
+# Initializing the event list. At first the only active event is that of the next arrival.
+# As passengers enter the plane, they too get their separate event.
+events = [{'ID': 'Arrival', 'timer' : random.expovariate(0.5)}]
+
+
+
+#########################################################
+# Identifying the next event 
+
+min_timer = min(events, key = lambda x: x['timer'])['timer']    # Find the time to the next event
+
+# Retrieve the event list index of the next event (by matching the smallest timer)
+action_index = next((index for (index, d) in enumerate(events) if d["timer"] == min_timer), None) 
+
+current_time = current_time + min_timer # Update current time
+
+for i in range(len(events)):          # Update the other event timers.
+    events[i]['timer'] = events[i]['timer'] - min_timer
+    
+    
+    
+# Executing next event
+
+# First we define what happens if the event is a new arrival
+if events[action_index]['ID'] == 'Arrival':
+    # In case the first spot on the aisle is occupied, the arrival is delayed.
+    if bool(aisle[0]):
+        events[action_index]['timer'] = 1   # Setting the delay arbitrarily to 1 second, NEED TO DISCUSS AND UPDATE.
+    else:
+        events[action_index]['timer'] = random.expovariate(0.5) # Otherwise draw from an exp. distribution with mean 2 ALSO NEED TO DISCUSS.
+        New_arrival(passengers, aisle, current_time)    # And let the new passenger arrive.
+    # If all passengers are in the plane, remove the arrival event from the event list.        
+    if all(d['aisle_pos'] is not None for d in passengers):
+        del events[action_index]
+    
+else: # If we are not dealing with a new arrival, we must be dealing with a passenger.
+
+    # Designate the passenger that is going to move.
+    pass_index = next((index for (index, d) in enumerate(passengers) if d["ID"] == events[action_index]['ID']), None)
+    
+    # First we check if the passenger has been seated. If so, we remove the passenger from the event list
+    if passengers[pass_index]['seated'] == True:
+        del events[action_index]
+        print(f"Passenger {passengers[pass_index]['ID']} has been seated!")
+    else:    
+    
+        aisle_position = passengers[pass_index]['aisle_pos']    # Retrieve his position on the aisle.
+        
+        # If the next space on the aisle is occupied, the passenger needs to wait.
+        # The way it is coded here, the waiting time is basically the movement resetting. I think this is the right way to go.
+        if bool(aisle[aisle_position + 1]):
+            # Taking into account smaller tiles at the beginning of the aisle.
+            if aisle_position < l_aisle - nrows:    
+                # Schedule next movement.
+                events[action_index]['timer'] = passengers[pass_index]['walkspeed']['aisle_begin']
+                # Update waiting time.
+                passengers[pass_index]['timespent']['waiting'] = passengers[pass_index]['timespent']['waiting'] + passengers[pass_index]['walkspeed']['aisle_begin']
+            # Now for the larger aisle tiles at the seats.
+            else:           
+                events[action_index]['timer'] = passengers[pass_index]['walkspeed']['aisle_seats']
+                passengers[pass_index]['timespent']['waiting'] = passengers[pass_index]['timespent']['waiting'] + passengers[pass_index]['walkspeed']['aisle_seats']
+        
+        # If the next space on the aisle is vacant, the passenger moves.
+        else:
+            aisle[aisle_position] = 0
+            aisle[aisle_position + 1] = 1
+            passengers[pass_index]['aisle_pos'] = aisle_position + 1
+            
+            # If he has not arrived at his row number yet, we simply schedule his next movement.
+            if not passengers[pass_index]['aisle_pos'] == passengers[pass_index]['row']:
+                # Again accounting for different tile sizes. Some duplicate code cannot be avoided here.
+                if aisle_position < l_aisle - nrows:    
+                    events[action_index]['timer'] = passengers[pass_index]['walkspeed']['aisle_begin']
+                else: 
+                    events[action_index]['timer'] = passengers[pass_index]['walkspeed']['aisle_seats']
+            
+            # Now for something very important: if the passenger arrives at the row of his seat he needs to sit!!!!!! FCKKKK
+            else:
+                # !!!!!!!!!!!!!!!!!!!       READ LINE BELOW     !!!!!!!!!!!!!!!!!!!
+                # The SeatingManager function needs to be updated such that it returns the time it takes for the passenger to get seated.
+                # This means that the SeatingManager also needs to incorporate luggage delay.
+                # events[action_index]['timer'] = SeatingManager(passengers[pass_index], seating_matrix)
+                passengers[pass_index]['seated'] == True
+
+
+
+
+
